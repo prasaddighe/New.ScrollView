43a16f989487728bf4aae0f181d17d53160c6612
prasaddighe <134702507+prasaddighe@users.noreply.github.com>
2023-08-16 16:07:32 +0530

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,288 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return DemoAppState();
+  }
+}
+
+class DemoAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: ListView(
+          scrollDirection: Axis.horizontal,
+          children: [
+            Container(
+              height: 200,
+              child: Text("Prasad"),
+              color: Color.fromARGB(255, 248, 149, 0),
+            ),
+            SizedBox(
+              width: 50,
+            ),
+            Container(
+              height: 200,
+              child: Text("Prasad"),
+              color: const Color.fromARGB(255, 255, 255, 255),
+            ),
+            SizedBox(
+              width: 50,
+            ),
+            Container(
+              height: 200,
+              child: Text("Prasad"),
+              color: Color.fromARGB(255, 4, 88, 0),
+            ),
+            SizedBox(
+              width: 50,
+            ),
+            Container(
+              height: 200,
+              child: Text("Prasad"),
+              color: Color.fromARGB(255, 21, 4, 255),
+            ),
+            SizedBox(
+              width: 50,
+            ),
+            Container(
+              height: 200,
+              child: Text("Prasad"),
+              color: Color.fromARGB(255, 255, 4, 4),
+            ),
+            SizedBox(
+              width: 50,
+            ),
+            Container(
+              height: 200,
+              child: Text("Prasad"),
+              color: Color.fromARGB(255, 247, 4, 255),
+            ),
+            SizedBox(
+              width: 50,
+            ),
+            Container(
+              height: 200,
+              child: Text("Prasad"),
+              color: Color.fromARGB(255, 255, 4, 75),
+            ),
+            SizedBox(
+              width: 50,
+            ),
+            Container(
+              height: 200,
+              child: Text("Prasad"),
+              color: Color.fromARGB(255, 4, 142, 255),
+            ),
+            SizedBox(
+              width: 50,
+            ),
+            Container(
+              height: 200,
+              child: Text("Prasad"),
+              color: Color.fromARGB(255, 4, 255, 205),
+            ),
+            SizedBox(
+              width: 50,
+            ),
+            Container(
+              height: 200,
+              child: Text("Prasad"),
+              color: Color.fromARGB(255, 192, 255, 4),
+            ),
+            SizedBox(
+              width: 50,
+            ),
+            Container(
+              height: 200,
+              child: Text("Prasad"),
+              color: Color.fromARGB(255, 163, 255, 4),
+            ),
+            SizedBox(
+              width: 50,
+            ),
+            Container(
+              height: 200,
+              child: Text("Prasad"),
+              color: Color.fromARGB(255, 255, 121, 4),
+            ),
+            SizedBox(
+              width: 50,
+            ),
+            Container(
+              height: 200,
+              child: Text("Prasad"),
+              color: Color.fromARGB(255, 93, 86, 81),
+            ),
+            SizedBox(
+              width: 50,
+            ),
+            Container(
+              height: 200,
+              child: Text("Prasad"),
+              color: Color.fromARGB(255, 45, 43, 40),
+            ),
+            SizedBox(
+              width: 50,
+            ),
+            Container(
+              height: 200,
+              child: Text("Prasad"),
+              color: Color.fromARGB(255, 60, 44, 30),
+            ),
+            SizedBox(
+              width: 50,
+            ),
+            Container(
+              height: 200,
+              child: Text("Prasad"),
+              color: Color.fromARGB(255, 42, 22, 5),
+            ),
+            SizedBox(
+              width: 50,
+            ),
+            Container(
+              height: 200,
+              child: Text("Prasad"),
+              color: Color.fromARGB(255, 39, 19, 1),
+            ),
+            SizedBox(
+              width: 50,
+            ),
+            Container(
+              height: 200,
+              child: Text("Prasad"),
+              color: Color.fromARGB(255, 96, 46, 1),
+            ),
+            SizedBox(
+              width: 50,
+            ),
+            Container(
+              height: 200,
+              child: Text("Prasad"),
+              color: Color.fromARGB(255, 4, 255, 113),
+            ),
+            SizedBox(
+              width: 50,
+            ),
+            Container(
+              height: 200,
+              child: Text("Prasad"),
+              color: Color.fromARGB(255, 4, 255, 238),
+            ),
+            SizedBox(
+              width: 50,
+            ),
+            Container(
+              height: 200,
+              child: Text("Prasad"),
+              color: Color.fromARGB(255, 255, 121, 4),
+            ),
+            SizedBox(
+              width: 50,
+            ),
+            Container(
+              height: 200,
+              child: Text("Prasad"),
+              color: Color.fromARGB(255, 255, 121, 4),
+            ),
+            SizedBox(
+              width: 50,
+            ),
+            Container(
+              height: 200,
+              child: Text("Prasad"),
+              color: Color.fromARGB(255, 255, 121, 4),
+            ),
+            SizedBox(
+              width: 50,
+            ),
+            Container(
+              height: 200,
+              child: Text("Prasad"),
+              color: Color.fromARGB(255, 255, 121, 4),
+            ),
+            SizedBox(
+              width: 50,
+            ),
+            Container(
+              height: 200,
+              child: Text("Prasad"),
+              color: Color.fromARGB(255, 255, 121, 4),
+            ),
+            SizedBox(
+              width: 50,
+            ),
+            Container(
+              height: 200,
+              child: Text("Prasad"),
+              color: Color.fromARGB(255, 255, 121, 4),
+            ),
+            SizedBox(
+              width: 50,
+            ),
+            Container(
+              height: 200,
+              child: Text("Prasad"),
+              color: Color.fromARGB(255, 255, 121, 4),
+            ),
+            SizedBox(
+              width: 50,
+            ),
+            Container(
+              height: 200,
+              child: Text("Prasad"),
+              color: Color.fromARGB(255, 255, 121, 4),
+            ),
+            SizedBox(
+              width: 50,
+            ),
+            Container(
+              height: 200,
+              child: Text("Prasad"),
+              color: Color.fromARGB(255, 255, 121, 4),
+            ),
+            SizedBox(
+              width: 50,
+            ),
+            Container(
+              height: 200,
+              child: Text("Prasad"),
+              color: Color.fromARGB(255, 255, 121, 4),
+            ),
+            SizedBox(
+              width: 50,
+            ),
+            Container(
+              height: 200,
+              child: Text("Prasad"),
+              color: Color.fromARGB(255, 255, 121, 4),
+            ),
+            SizedBox(
+              width: 50,
+            ),
+            Container(
+              height: 200,
+              child: Text("Prasad"),
+              color: Color.fromARGB(255, 255, 121, 4),
+            ),
+            SizedBox(
+              width: 50,
+            ),
+            Container(
+              height: 200,
+              child: Text("Prasad"),
+              color: Color.fromARGB(255, 44, 26, 10),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
